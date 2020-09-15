1d22a5e9f59a348ff429f486e36eae145b3ec662
Your Name <nusret-24@hotmail.co.uk>
2020-09-15 16:24:29 +0200

    some added navigation

--- app/src/main/java/com/example/todo_app_mobile/TaskManager.java
+++ app/src/main/java/com/example/todo_app_mobile/TaskManager.java
@@ -2,7 +2,10 @@ package com.example.todo_app_mobile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class TaskManager extends AppCompatActivity {
 
@@ -10,5 +13,45 @@ public class TaskManager extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task_manager);
+
+        Button finish1 = (Button)findViewById(R.id.t1finish);
+
+        finish1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                startActivity(new Intent(TaskManager.this, WorkTime.class));
+            }
+        });
+
+        Button finish2 = (Button)findViewById(R.id.t2finish);
+
+        finish2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                startActivity(new Intent(TaskManager.this, WorkTime.class));
+            }
+        });
+
+        Button finish3 = (Button)findViewById(R.id.t3finish);
+
+        finish3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                startActivity(new Intent(TaskManager.this, WorkTime.class));
+            }
+        });
+
+        Button finish4 = (Button)findViewById(R.id.t4finish);
+
+        finish4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                startActivity(new Intent(TaskManager.this, WorkTime.class));
+            }
+        });
     }
 }
--- app/src/main/java/com/example/todo_app_mobile/WorkTime.java
+++ app/src/main/java/com/example/todo_app_mobile/WorkTime.java
@@ -2,13 +2,29 @@ package com.example.todo_app_mobile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class WorkTime extends AppCompatActivity {
+
+    TextView TotalTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_work_time);
+
+        Button send = (Button)findViewById(R.id.send);
+
+        send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                startActivity(new Intent(WorkTime.this, TaskManager.class));
+            }
+        });
     }
 }
