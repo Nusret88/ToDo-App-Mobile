ffc6db990bfe69cae1760792dc5243f63cd75d16
Your Name <nusret-24@hotmail.co.uk>
2020-09-15 13:19:25 +0200

    login screen design

--- app/src/main/java/com/example/todo_app_mobile/WorkTime.java
+++ app/src/main/java/com/example/todo_app_mobile/WorkTime.java
@@ -4,13 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class WorkTime extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-
+        setContentView(R.layout.activity_work_time);
     }
 }
