dd3ca02c9f3d838705779f7eda7e66ad24129e90
Your Name <nusret-24@hotmail.co.uk>
2020-09-17 11:19:40 +0200

    some fixes to the login interface and added calculate button and function to WorkTime class

--- app/src/main/java/com/example/todo_app_mobile/WorkTime.java
+++ app/src/main/java/com/example/todo_app_mobile/WorkTime.java
@@ -6,11 +6,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class WorkTime extends AppCompatActivity {
 
-    TextView TotalTime;
+    TextView totaltime;
+    EditText overtime,inctime,traveltime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +20,15 @@ public class WorkTime extends AppCompatActivity {
         setContentView(R.layout.activity_work_time);
 
         Button send = (Button)findViewById(R.id.send);
+        Button calc = (Button)findViewById(R.id.calc);
+        calc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                TotalTimeCalc();
+            }
+        });
+
 
         send.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -27,4 +38,21 @@ public class WorkTime extends AppCompatActivity {
             }
         });
     }
+
+    public void TotalTimeCalc () {
+
+        totaltime = findViewById(R.id.totaltime);
+
+        overtime = findViewById(R.id.overtime);
+        inctime = findViewById(R.id.inctime);
+        traveltime = findViewById(R.id.traveltime);
+
+        int overT = Integer.parseInt(overtime.getText().toString());;
+        int inc = Integer.parseInt(inctime.getText().toString());;
+        int travel = Integer.parseInt(traveltime.getText().toString());;
+
+        totaltime.setText(overT + inc + travel + " minutes");
+    }
+
+
 }
