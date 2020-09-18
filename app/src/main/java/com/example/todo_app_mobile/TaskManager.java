8ed65a578b7f3820d19af529de8edc535f5e7f33
Your Name <nusret-24@hotmail.co.uk>
2020-09-18 18:21:24 +0200

    added tablerow array and button array

--- app/src/main/java/com/example/todo_app_mobile/TaskManager.java
+++ app/src/main/java/com/example/todo_app_mobile/TaskManager.java
@@ -8,6 +8,7 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TableRow;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -25,6 +26,8 @@ public class TaskManager extends AppCompatActivity {
     HashMap<Integer, String> taskList;
     JSONArray jsonArray;
     int operatorID;
+    TableRow row1, row2, row3, row4, row5, row6;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,9 +42,33 @@ public class TaskManager extends AppCompatActivity {
         asyncTaskAPI = new RestConnectionTask();
         asyncTaskAPI.execute("https://taskmanager2020-api.herokuapp.com/api/tasks/" + operatorID);
 
+        final Button btnList[] = new Button[18];
+        int[] btnId = {R.id.t1accept, R.id.t2accept, R.id.t3accept, R.id.t4accept, R.id.t5accept, R.id.t6accept,
+                R.id.t1decline, R.id.t2decline, R.id.t3decline, R.id.t4decline, R.id.t5decline, R.id.t6decline,
+                R.id.t1finish, R.id.t2finish, R.id.t3finish, R.id.t4finish, R.id.t5finish, R.id.t6finish};
 
-        Button finish1 = (Button)findViewById(R.id.t1finish);
+        for (int i = 0; i < btnList.length; i++)
+            btnList[i] = (Button) findViewById(btnId[i]);
 
+        private View.OnClickListener btnListener = null for (Button btn : btnList)
+            btn.setOnClickListener(btnListener);
+
+        btnListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                for (int i = 0; i < btnList.length; i++) {
+                    
+                }
+            }
+        };
+
+        rowArray(2).setVisibility(View.INVISIBLE);
+        rowArray(3).setVisibility(View.INVISIBLE);
+        rowArray(4).setVisibility(View.INVISIBLE);
+        rowArray(5).setVisibility(View.INVISIBLE);
+
+
+        Button finish1 = (Button) findViewById(R.id.t1finish);
         finish1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -50,32 +77,26 @@ public class TaskManager extends AppCompatActivity {
             }
         });
 
-        Button finish2 = (Button)findViewById(R.id.t2finish);
-
+        Button finish2 = (Button) findViewById(R.id.t2finish);
         finish2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
                 startActivity(new Intent(TaskManager.this, WorkTime.class));
             }
         });
 
-        Button finish3 = (Button)findViewById(R.id.t3finish);
-
+        Button finish3 = (Button) findViewById(R.id.t3finish);
         finish3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
                 startActivity(new Intent(TaskManager.this, WorkTime.class));
             }
         });
 
-        Button finish4 = (Button)findViewById(R.id.t4finish);
-
+        Button finish4 = (Button) findViewById(R.id.t4finish);
         finish4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
                 startActivity(new Intent(TaskManager.this, WorkTime.class));
             }
         });
@@ -141,5 +162,25 @@ public class TaskManager extends AppCompatActivity {
                 e.printStackTrace();
             }
         }
+    }
+
+    public TableRow rowArray (int row) {
+        TableRow [] tr = new TableRow[6];
+
+        row1 = findViewById(R.id.row1);
+        row2 = findViewById(R.id.row2);
+        row3 = findViewById(R.id.row3);
+        row4 = findViewById(R.id.row4);
+        row5 = findViewById(R.id.row5);
+        row6 = findViewById(R.id.row6);
+
+        tr[0] = row1;
+        tr[1] = row2;
+        tr[2] = row3;
+        tr[3] = row4;
+        tr[4] = row5;
+        tr[5] = row6;
+
+        return tr[row];
     }
 }
